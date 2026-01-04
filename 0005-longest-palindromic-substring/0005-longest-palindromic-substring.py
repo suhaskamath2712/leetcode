1b50f9982b15af5cbc4c552dc544eaa46792069c
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-01-04 09:44:49 +0530

    Time: 706 ms (30.49%), Space: 11.8 MB (99.99%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        def expandCenterOdd(index):
+            i = index - 1
+            j = index + 1
+            palindrome = []
+            while i >= 0 and j < len(s):
+                if s[i] != s[j]:
+                    break
+                palindrome = s[i:j+1]
+
+                i -= 1
+                j += 1
+            
+            return palindrome
+
+        def expandCenterEven(index):
+            i = index - 1
+            j = index
+            palindrome = []
+            while i >= 0 and j < len(s):
+                if s[i] != s[j]:
+                    break
+                palindrome = s[i:j+1]
+
+                i -= 1
+                j += 1
+            return palindrome
+
+            
+        
+        max_palindrome = s[0]
+
+        for i in range(len(s)):
+            temp = expandCenterOdd(i)
+            max_palindrome = temp if len(temp) > len(max_palindrome) else max_palindrome
+            temp = expandCenterEven(i)
+            max_palindrome = temp if len(temp) > len(max_palindrome) else max_palindrome
+        
+        return max_palindrome
+
+        
