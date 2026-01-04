0f1807434519491eb38b4054fc487a4b6b7cc0cd
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-01-04 09:31:39 +0530

    Time: 2589 ms (9.14%), Space: 13.3 MB (30.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         rlist = []
         for i in range(len(nums)):
-            for j in range(len(nums)):
+            for j in range(i+1,len(nums)):
                 if j != i and nums[i] + nums[j] == target:
                     rlist.append(i)
                     rlist.append(j)
