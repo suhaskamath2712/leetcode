2dd206410d2d12d757bcf2765f93efbc31677459
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-04-13 12:29:15 +0530

    Time: 0 ms (100%), Space: 45.1 MB (15.64%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -14,45 +14,5 @@ class Solution {
                 minright = i - start;
 
         return (minleft < minright) ? minleft : minright;
-
-        /*
-        int i = 0;
-
-        //Explore in both directions
-        while (start-i >= 0 && start+i < nums.length)
-        {
-            if (nums[start-i] == target || nums[start+i] == target)
-                return i;
-            
-            i++;
-        }
-
-        System.out.println("Bidirectional search failed!");
-        
-        int temp = i;
-
-        //Explore left
-        while (start-temp >= 0)
-        {
-            if (nums[start-temp] == target)
-                return temp;
-            temp++;
-        }
-
-        System.out.println("Left search failed!");
-        
-        temp = i;
-
-        //Explore right
-        while (start+temp < nums.length)
-        {   
-            if (nums[start+temp] == target)
-                return temp;
-        }
-
-        System.out.println("Right search failed!");
-
-        return -1;
-        */
     }
 }
