8650579b77b45832ed317926bfc55403f5380189
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-04-24 18:49:30 +0530

    Time: 9 ms (83.82%), Space: 105 MB (97.06%) - LeetHub

---
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.java
@@ -0,0 +1,10 @@
+class Solution {
+        public long maxScore(int[] a, int[] b) {
+        long[] dp = new long[4];
+        Arrays.fill(dp, (long)-1e11);
+        for (int bi : b)
+            for (int i = 3; i >= 0; i--)
+                dp[i] = Math.max(dp[i], (i > 0 ? dp[i - 1] : 0) + 1L * a[i] * bi);
+        return dp[3];
+    }
+}
