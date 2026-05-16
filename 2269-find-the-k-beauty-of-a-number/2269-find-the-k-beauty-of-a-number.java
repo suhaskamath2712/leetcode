a94b8821f9cbd52a3378ece9b549612adce31cbc
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-05-16 16:49:22 +0530

    Time: 4 ms (6.05%), Space: 42.6 MB (6.15%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int divisorSubstrings(int num, int k)
+    {
+        List<Integer> k_grams = new ArrayList<>();
+        String num_str = Integer.toString(num);
+
+        for (int i = 0; i < num_str.length() + 1 - k; i++)
+            k_grams.add(Integer.parseInt(num_str.substring(i,i+k)));
+        
+        System.out.println(k_grams);
+
+        int beauty = 0;
+
+        for (int k_gram : k_grams)
+            if (k_gram != 0 && num % k_gram == 0)
+                beauty++;
+        
+        return beauty;
+    }
+}
