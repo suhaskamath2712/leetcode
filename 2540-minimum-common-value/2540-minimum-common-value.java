580d09a9c5f4252bc982b0233ac4acabffd84710
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-05-19 09:53:02 +0530

    Time: 536 ms (5.79%), Space: 86.7 MB (20.59%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -3,12 +3,17 @@ class Solution {
     {
         HashSet<Integer> num1Set = new HashSet<>();
 
-        for (int num : nums1)
-            num1Set.add(num);
-        
-        for (int num : nums2)
-            if (num1Set.contains(num))
-                return num;
+        for (int num1 : nums1)
+        {
+            num1Set.add(num1);
+            for (int num2 : nums2)
+            {
+                if (num1 < num2)
+                    break;
+                if (num1Set.contains(num2))
+                    return num2;
+            }
+        }
         
         return -1;
     }
