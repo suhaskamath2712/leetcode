9ab2a3c517a4290ab515ce07bdb9d738724acc17
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-05-28 17:48:56 +0530

    Time: 2 ms (91.88%), Space: 43.1 MB (52.01%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -4,26 +4,25 @@ import java.util.List;
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        if (nums.length == 0) {
+
+        if (nums.length == 0)
             return result;
-        }
         
         int i = 0;
-        while (i < nums.length) {
+        while (i < nums.length)
+        {
             int start = nums[i];
             int j = i;
             // Expand the range as long as elements are consecutive
-            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1) {
+            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1)
                 j++;
-            }
             
             // Format the range string
-            if (nums[j] == start) {
+            if (nums[j] == start)
                 result.add(String.valueOf(start));
-            } else {
+            else
                 result.add(start + "->" + nums[j]);
-            }
-            
+                        
             // Move to the next potential start of a range
             i = j + 1;
         }
