7810053e1ae07fae319a12c1bc3e6f25dc85867e
Suhas Kamath <93072974+suhaskamath2712@users.noreply.github.com>
2026-07-30 13:39:10 +0530

    Time: 1 ms (29.67%), Space: 43.4 MB (16.51%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -11,7 +11,12 @@ class Solution {
         Arrays.sort(freq);
 
         for (int i = 0; i < 26; i++)
+        {
             cnt += (i/8 + 1) * freq[26 - i - 1];
+
+            if (freq[26 - i - 1] == 0)
+                break;
+        }
         
         return cnt;
     }
