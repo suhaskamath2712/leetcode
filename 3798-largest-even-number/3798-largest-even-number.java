7b4ca10c8ca56e2ca078d76549d914db25cd3d82
Suhas <93072974+suhaskamath2712@users.noreply.github.com>
2026-08-17 19:44:07 +0530

    Time: 6 ms (2.42%), Space: 44.3 MB (11.13%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String largestEven(String s)
+    {
+        if ((s.charAt(s.length()-1) - '0') % 2 == 0)
+        {
+            System.out.println("here!");
+            return s;
+        }
+        
+        if (s.length() == 1)
+            return "";
+
+        int earlistIdx = Integer.MIN_VALUE;
+
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            if ((s.charAt(i)-'0') % 2 == 0)
+            {
+                earlistIdx = i;
+                break;
+            }
+        }
+
+        System.out.println(earlistIdx);
+
+        if (earlistIdx != Integer.MIN_VALUE)
+            return s.substring(0,earlistIdx+1);
+        
+        return "";
+    }
+}
